c8a306dace118bf7e032177e25875a22a2c2b539
Nicolas Saldeno <skyladdamebusiness@gmail.com>
2022-05-16 12:31:46 -0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import NavBar from './components/navbar/NavBar';
 
 function App() {
   return (
     <div className="App">
-    <h1>Project dionysus</h1>
+    <h1>Project Dionysus</h1>
+    <NavBar />
     </div>
   );
 }

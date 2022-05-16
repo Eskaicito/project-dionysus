268b583368207de444d87e9e18038f0bd6819b99
Nicolas Saldeno <skyladdamebusiness@gmail.com>
2022-05-16 14:07:34 -0300

    augmented ui added to navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,7 +1,25 @@
+import 'augmented-ui'
+import './NavBar.css'
+
 const NavBar = () => {
     return(
-        <div>
-            NavBar
+        <div className='mixin-set'>
+            <nav data-augmented-ui="inlay tl-2-clip-x br-2-clip-x" className='navbar'>
+                <ul>
+                    <li>
+                        <button>Sexo</button>
+                    </li>
+                    <li>
+                        <button>Sexo</button>
+                    </li>
+                    <li>
+                        <button>Sexo</button>
+                    </li>
+                    <li>
+                        <button>Sexo</button>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 } 

e968922c9f0cc41ae9b51f8cdd7dae2bf5b1bc04
Nicolas Saldeno <skyladdamebusiness@gmail.com>
2022-05-18 15:37:08 -0300

    items folder added

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -3,13 +3,7 @@ import './Banner.css'
 const Banner = () =>{
     return(
         <>
-        <div className='container'>
-            <div className='banner'>
-                <div className='banner-container'>
-                <div className='logo'><img src='dionysus.png'></img>PROJECT DIONYSUS</div>
-                </div>
-            </div>
-        </div>
+        <div className='banner'></div>
         </>
     )
 }

f8551428dbfb6f78d53d925a11ed9bbf31c76766
Nicolas Saldeno <skyladdamebusiness@gmail.com>
2022-05-29 18:54:07 -0300

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import Scanlines from '../components/Scanlines/Scanlines';
 const Home = () => {
     return (
         <>
-        <h1>Home</h1>
+        <h1 style={{color: "white"}}>Home</h1>
         <Scanlines />
         </>
     )
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -4,6 +4,7 @@ const Products = () => {
     return (
 
         <>
+        <h1 style={{color: "white"}}>Products</h1>
             <Scanlines />
             <ItemsListContainer />
         </>

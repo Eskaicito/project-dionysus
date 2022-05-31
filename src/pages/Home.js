36460cae0cb240bbae748bf48cc38ab90c05e067
Nicolas Saldeno <skyladdamebusiness@gmail.com>
2022-05-31 16:13:02 -0300

    homepage stylized

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,14 @@ import Scanlines from '../components/Scanlines/Scanlines';
 const Home = () => {
     return (
         <>
-        <h1 style={{color: "white"}}>Home</h1>
-        <Scanlines />
+            <h1 style={{ color: "white" }}>Home</h1>
+            <div>
+                <img style={{ position: "relative", width: "40vw", height:"50vh" }} src='../chess_pattern.png' alt='dionysus_home'></img>
+                <img style={{ position: "absolute", top:"290px", left:"550px",width: "40vw" }} src='../dionysus_98.png' alt='dionysus_home'></img>
+                <img style={{ position: "absolute", top:"230px", left:"10px",width: "30vw" }} src='../message_hello.png' alt='dionysus_home'></img>
+                <img style={{ position: "absolute", top:"340px", left:"10px",width: "30vw" }} src='../message_welcome.png' alt='dionysus_home'></img>
+            </div>
+            <Scanlines />
         </>
     )
 }
